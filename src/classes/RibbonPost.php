75cd184a1b17039df05c70023a6ba7e7a45e41d8
taophp <steph@stephanemourey.fr>
2018-09-28 13:38:43 +0200

    Writing works now

--- src/classes/RibbonPost.php
+++ src/classes/RibbonPost.php
@@ -13,51 +13,61 @@
  */
 class RibbonPost {
     const DATE_FORMAT_4_FILE_NAME = 'Y-m-d-';
-    const DATE_FORM_4_YAML = 'Y-m-d H:i:s';
+    const DATE_FORMAT_4_YAML = 'Y-m-d H:i:s';
+    const YAML_SEPARATOR = PHP_EOL.'---'.PHP_EOL;
     
     protected $postsSourceDirectory;
-    protected $fileName;
-    protected $yaml;
-    protected $content;
-    protected $timestamp;
     protected $title;
+    protected $content;
+    //protected $fileName;
+    //protected $yaml;
+    //protected $content;
+    //protected $timestamp;
     
     public function __construct(string $dir) {
+        if (!is_dir($dir) || !is_writable($dir)) {
+            throw new Exception($dir.' MUST be writable directory !');
+        }
         $this->postsSourceDirectory = $dir;
     }
     
-    /*function __get($name) {
+    function __get($name) {
         if (!isset($this->$name)) {
             $method = '_get' . ucfirst($name);
-            if (method_exists($this, $method))
+            if (method_exists($this, $method)) {
                 $this->$method();
+            }
             return $this->$name;
         }
-    }*/
+    }
 
-    public function save(string $content) : void {
-        /** @todo What if the file already exists ? */
-        $this->timestamp = time();
-        $this->parseContent($content);
-        file_put_contents($this->postsSourceDirectory.'/'.$this->fileName, $this->yaml.$this->content);
+    protected function _getFileName() : void {
+        $this->fileName = date(static::DATE_FORMAT_4_FILE_NAME,$this->timestamp).urlencode($this->title).'.md';
+    }
+    
+    protected function _getFileFullName() : void {
+        $this->fileFullName = $this->postsSourceDirectory.'/'.$this->fileName = urlencode($this->title).'md';
     }
     
     protected function _getTimestamp()  {
         $this->timestamp = time();
     }
-    
-    protected function parseContent(string $content) : void {
-        list ($this->title,$this->content) = explode(PHP_EOL,$content,2);
-    }
-    
-    protected function setFileName() {
-        $this->fileName = date(static::DATE_FORMAT_4_FILE_NAME,$this->timestamp).urlencode(trim($this->title)).'.md';        
-    }
-    
-    protected function setYaml() {
+
+    protected function _getYaml() {
         $this->yaml = 'title: '.$this->title. PHP_EOL
-                        . 'date: '.date(static::DATE_FORM_4_YAML,$this->timestamp). PHP_EOL
-                        . '---'. PHP_EOL;        
+                        . 'date: '.date(static::DATE_FORMAT_4_YAML,$this->timestamp);        
+    }
+    
+    public function save(string $content) : void {
+        /** @todo What if the file exists ? */
+        $this->parseContentFromForm($content);
+        file_put_contents($this->postsSourceDirectory.'/'.$this->fileName,
+                          $this->yaml.static::YAML_SEPARATOR.$this->content
+        );
+    }
+    
+    protected function parseContentFromForm(string $content) : void {
+        list ($this->title,$this->content) = explode(PHP_EOL,$content,2);
     }
     
     public function html() {
