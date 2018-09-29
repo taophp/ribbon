df1a9a1e415b3d157936e7870deb1cdf6e278a85
Stéphane Mourey <stephane.mourey@impossible-exil.info>
2018-09-29 20:14:56 +0200

    Work in progress

---
+++ src/classes/RibbonGenerator.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of RibbonGenerator
+ *
+ * @author Stéphane Mourey <steph@stephanemourey.fr>
+ */
+class RibbonGenerator {
+    public function __contruct(string $postsDirectory, string $targetFile) {
+        
+    }
+    
+    public function generate() {
+        
+    }
+}
