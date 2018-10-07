683602538c78528ae4cbc928150be2d512409f8c
Stéphane Mourey <stephane.mourey@impossible-exil.info>
2018-10-07 17:59:56 +0200

    Align text-title on the left

--- src/classes/RibbonPostReader.php
+++ src/classes/RibbonPostReader.php
@@ -35,7 +35,7 @@ class RibbonPostReader {
         list ($yamlstring,$markdown) = explode(RibbonPostWritter::YAML_SEPARATOR,$fileContent,2);
         $this->yaml = Yaml::parse($yamlstring);
         $mdParser = new \cebe\markdown\GithubMarkdown();
-        $this->title = $mdParser->parse($this->yaml['title']);
+        $this->title = str_replace(['<p>','</p>'],'',$mdParser->parse($this->yaml['title']));
         $this->date = $this->yaml['date']-$offset;
         $this->content = $mdParser->parse($markdown);
     }
